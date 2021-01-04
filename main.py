2bd4e1d20dc17b2b2c87c6a3c40cc700b44290d8
Joseph-Schtein <yossiand10@gmail.com>
2021-01-04 10:33:10 +0200

    giving values need to check if they are correct

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def run_hc():
     CSV_COLUMN_NAMES = ['BodyTemp', 'Gender', 'Weight']
     df = pd.read_csv(path, sep='   ' or '    ', engine='python', names=CSV_COLUMN_NAMES, header=0)
     df = df.replace({'Gender': {2: 0}})
-    results_list = np.zeros(shape=df.shape[0])
+    results_list = np.zeros(shape=(5, 3))
+    for i in range(50):
+        results_list += algorithm(df, 'BodyTemp', 'Weight', 'Gender')
 
-    for i in range(100):
-        results_list[i]=algorithm(df, 'BodyTemp', 'Weight', 'Gender')
+    for i in range(5):
+        for j in range(3):
+            results_list[i][j] = results_list[i][j]/50
 
     return results_list
 
@@ -24,8 +27,6 @@ def algorithm(df, x, y, label):
     train_values_y = list(train[y])
     test_values_x = list(test[x])
     test_values_y = list(test[y])
-    train_size = train.shape[0]
-    test_size = test.shape[0]
 
     train_labels = list(train[label])
     test_labels = list(test[label])
@@ -33,28 +34,28 @@ def algorithm(df, x, y, label):
     test_prediction_p2 = np.zeros(shape=65)
     test_prediction_infinity = np.zeros(shape=65)
 
-    output = np.zero(shape=(5, 3))
-
+    output = np.zeros(shape=(5, 3))
+    index = 0
     for k in [1, 3, 5, 7, 9]:
-        manh_dis = np.zero(shape=k)
-        euc_dis = np.zero(shape=k)
-        infinity_dis = np.zero(shape=k)
+        manh_dis = np.zeros(shape=k)
+        euc_dis = np.zeros(shape=k)
+        infinity_dis = np.zeros(shape=k)
 
-        value_closer_p1 = np.zero(shape=(k, 3))
-        value_closer_p2 = np.zero(shape=(k, 3))
-        value_closer_infinity = np.zero(shape=(k, 3))
+        value_closer_p1 = np.zeros(shape=(k, 3))
+        value_closer_p2 = np.zeros(shape=(k, 3))
+        value_closer_infinity = np.zeros(shape=(k, 3))
 
-        for i in range(65): # Passing on all the test data for the algorithm
+        for i in range(64): # Passing on all the test data for the algorithm
             dot_x = test_values_x[i]
             dot_y = test_values_y[i]
-            label = test_labels[i]
+            label = train_labels[i]
             initialize = True
-            for j in range(65): # Comput the distance according to l1, l2 and l-infinity over all the train values
+            for j in range(64): # Comput the distance according to l1, l2 and l-infinity over all the train values
                 tmp_distance_for_p1 = abs(dot_x - train_values_x[j]) + abs(dot_y - train_values_y[j])
                 tmp_distance_for_p2 = math.sqrt(math.pow(dot_x - train_values_x[j], 2) + math.pow(dot_y - train_values_y[j], 2))
                 tmp_distance_for_infinity = max(abs(dot_x - train_values_x[j]), abs(dot_y - train_values_y[j]))
 
-                tmp_dot = np.zero(shape=3)
+                tmp_dot = np.zeros(shape=3)
 
                 if not initialize:
                     for r in range(k):
@@ -67,8 +68,8 @@ def algorithm(df, x, y, label):
                             value_closer_p1[r][0] = dot_x
                             value_closer_p1[r][1] = dot_y
                             value_closer_p1[r][2] = label
-                            if r+1 < k:
-                                value_closer_p1[r+1] = tmp_dot
+                            #if r+1 < k:
+                            #    value_closer_p1[r+1] = tmp_dot
 
                 else:
                     manh_dis[j] = tmp_distance_for_p1
@@ -76,7 +77,7 @@ def algorithm(df, x, y, label):
                     value_closer_p1[j][1] = dot_y
                     value_closer_p1[j][2] = label
 
-                tmp_dot = np.zero(shape=3)
+                tmp_dot = np.zeros(shape=3)
 
                 if not initialize:
                     for r in range(k):
@@ -98,7 +99,7 @@ def algorithm(df, x, y, label):
                     value_closer_p2[j][1] = dot_y
                     value_closer_p2[j][2] = label
 
-                tmp_dot = np.zero(shape=3)
+                tmp_dot = np.zeros(shape=3)
 
                 if not initialize:
                     for r in range(k):
@@ -120,16 +121,17 @@ def algorithm(df, x, y, label):
                     value_closer_infinity[j][1] = dot_y
                     value_closer_infinity[j][2] = label
 
-                if k == j:
+                if k == j+1:
                     initialize = False
 
+            zeros_p1 = 0
+            ones_p1 = 0
+            zeros_p2 = 0
+            ones_p2 = 0
+            zeros_infinity = 0
+            ones_infinity = 0
+
             for j in range(k):  # Label the dot
-                zeros_p1 = 0
-                ones_p1 = 0
-                zeros_p2 = 0
-                ones_p2 = 0
-                zeros_infinity = 0
-                ones_infinity = 0
 
                 if value_closer_p1[j][2] == 0:
                     zeros_p1 = zeros_p1+1
@@ -149,14 +151,54 @@ def algorithm(df, x, y, label):
                 else:
                     ones_infinity = ones_infinity + 1
 
+            if zeros_p1 > ones_p1:
+                test_prediction_p1[i] = 0
 
+            else:
+                test_prediction_p1[i] = 1
+
+            if zeros_p2 > ones_p2:
+                test_prediction_p2[i] = 0
+
+            else:
+                test_prediction_p2[i] = 1
+
+            if zeros_infinity > ones_infinity:
+                test_prediction_infinity[i] = 0
+
+            else:
+                test_prediction_infinity[i] = 1
+
+        mistake_p1 = 0
+        mistake_p2 = 0
+        mistake_infinity = 0
+
+        for i in range(65):
+
+            if test_labels[i] != test_prediction_p1[i]:
+                mistake_p1 = mistake_p1 + 1
+
+            if test_labels[i] != test_prediction_p2[i]:
+                mistake_p2 = mistake_p2 + 1
+
+            if test_labels[i] != test_prediction_infinity[i]:
+                mistake_infinity = mistake_infinity + 1
+
+        output[index][0] = mistake_p1 / 65
+        output[index][1] = mistake_p2 / 65
+        output[index][2] = mistake_infinity / 65
+        index = index+1
 
     return output
 
 
 def main():
-    run_hc()
-
+    results = run_hc()
+    print(results[0][0], ' ', results[0][1], ' ', results[0][2])
+    print(results[1][0], ' ', results[1][1], ' ', results[1][2])
+    print(results[2][0], ' ', results[2][1], ' ', results[2][2])
+    print(results[3][0], ' ', results[3][1], ' ', results[3][2])
+    print(results[4][0], ' ', results[4][1], ' ', results[4][2])
 
 if __name__ == '__main__':
     main()
